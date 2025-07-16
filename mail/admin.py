78bdf4b1ed0b4ceeb4231c1ee84e724c1c2cc81e
Ruslan Gilmanov <r_gilmanov2005@hotmail.com>
2025-07-16 22:27:17 +1000

    Added class User to admin.py

--- mail/admin.py
+++ mail/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import User
 
 # Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'username', 'email', 'date_joined')
+    ordering = ['date_joined', 'username']
+
+
+admin.site.register(User, UserAdmin)
